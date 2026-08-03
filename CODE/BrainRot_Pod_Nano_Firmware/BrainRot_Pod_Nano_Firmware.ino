da21acd1fe50185dbbef7ea69041bab8e1c450ec
Parth Dhiman <parthdhiman.pd@gmail.com>
2026-08-03 21:36:52 +0530

    Create BrainRot_Pod_Nano_Firmware.ino

---
+++ CODE/BrainRot_Pod_Nano_Firmware/BrainRot_Pod_Nano_Firmware.ino
@@ -0,0 +1,11 @@
+// Sample firmware for Brain_Rot_Pod_Nano
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
